152835d8842cecb95d673aa18136d349f72f1966
Landon <84143557+zhaoxuzy@users.noreply.github.com>
2026-09-02 09:22:13 +0800

    Create motivation.py

---
+++ src/collectors/motivation.py
@@ -0,0 +1,7 @@
+def collect_motivation(match, home_team, away_team):
+    """阶段4：战意指数"""
+    return {
+        "主队加权总分": None, "客队加权总分": None,
+        "主队子分": {}, "客队子分": {},
+        "主队依据摘要": {}, "客队依据摘要": {}
+    }
