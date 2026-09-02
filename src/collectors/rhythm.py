2e96fec782915e557044174ec0e0d5975e15339d
Landon <84143557+zhaoxuzy@users.noreply.github.com>
2026-09-02 09:22:25 +0800

    Create rhythm.py

---
+++ src/collectors/rhythm.py
@@ -0,0 +1,11 @@
+def collect_rhythm(team_name_en):
+    """阶段5：节奏数据"""
+    return {
+        "历史上下半场进球占比": None,
+        "近10场逆转次数": None,
+        "近10场被逆转次数": None,
+        "统计范围": None,
+        "来源": None,
+        "来源URL": None,
+        "查询时间": None
+    }
